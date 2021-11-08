78250847d81328e0b80710277b1f29bfb87ae916
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-08 16:45:55 -0600

    first commit

---
+++ ConstructorsBasicsCs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConstructorsBasicsCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
